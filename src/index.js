f190fa3321acce89bbe9cf04123a5a101e3f6876
Luana Cassamasso <luana.cassamasso@aluno.senai.br>
2023-12-06 15:00:12 -0300

    Alteracao da pasta

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import soma from './calc.js';
+import Heading from './components/heading.js';
+
+console.log("testando o webpack");
+
+soma(2,4);
+soma(10,20);
+
+//Loaders
+const heading = new Heading();
+
+heading.create("Este título é dinamico");
+heading.create("Hot reload");
